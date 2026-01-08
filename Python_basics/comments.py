cfe880dbc9134b89647ff84f0fdde6fbe0e30f2f
Munavvara-17 <151653262+Munavvara-17@users.noreply.github.com>
2026-01-08 22:10:37 +0530

    message

---
+++ Python_basics/comments.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+# This is a single-line comment (# symbol)
+
+
+"""
+This is a multi-line comment
+that spans multiple lines.
+It is often used for documentation. ("""
+or ''' single quotes)."""
+
+
